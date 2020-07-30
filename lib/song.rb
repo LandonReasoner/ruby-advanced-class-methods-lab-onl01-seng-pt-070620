b2b103c0f0ed948b807dc0b91a3099bf4e3c78df
Landon Reasoner <reasoner.landon90@gmail.com>
2020-07-30 22:42:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+  
   @@all = []
 
   def self.all
@@ -10,4 +11,36 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name 
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.all.find {|artist| artist.name == name}
+      self.all.find {|artist| artist.name == name}
+    else
+      new_artist = Artist.new(name)
+      new_artist
+    end
+  end
+  
 end
