2aaa7fe8a76520cfb44fe6fe61068b0cc56d056a
Landon Reasoner <reasoner.landon90@gmail.com>
2020-07-30 22:53:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.all.find {|song| song.name == name}
+    if self.all.find {|song| song.name == name} =
       self.find_by_name(name)
     else
       self.create_by_name(name)
