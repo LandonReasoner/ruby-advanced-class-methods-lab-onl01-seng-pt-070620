588520a4231712b4115eb5c9c96fd25a3a4512c2
Landon Reasoner <reasoner.landon90@gmail.com>
2020-07-30 23:13:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,11 +31,11 @@ class Song
   end
   
   def self.find_by_name(name)
-    @@all.find {|song| song.name == name}
+    self.all.find {|song| song.name == name}
   end
   
   def self.find_or_create_by_name(name)
-    self.all.find {|song| song.name = name} || self.create_by_name
+    self.find_by_name(name) || self.create_by_name(name)
   end
   
 end
